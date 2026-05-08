fee4b2cd30a445a9146885ac0bda1a73cb9f2c6c
SuperJK55 <stepan.kolenkin@mail.ru>
2026-05-08 12:29:03 +0600

    Initial Commit

---
+++ PetFlow-Mobile/PetFlow_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetFlow_MobileApp.swift
+//  PetFlow-Mobile
+//
+//  Created by Stepan Kolenkin on 08.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct PetFlow_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
